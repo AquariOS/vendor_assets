08a345de0526b4793bd20a8e72b95562d8c5d799
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-12 16:07:22 -0300

    assets: Import modded Longshot app
    Change-Id: I42becbdbec78e6e8305e3a5c08beb19002d28e9a

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,6 @@
 # Other makefiles
 include vendor/assets/config/sounds.mk
+include vendor/assets/config/packages.mk
 
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/assets/overlay/common/
@@ -9,3 +10,7 @@ PRODUCT_PACKAGES += \
     bootanimation.zip \
     charger_res_images \
     product_charger_res_images
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/assets/permissions/custom-privapp-permissions-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/custom-privapp-permissions.xml \
