79afb2dd21b7d3c2dc744076bab0e14ee3b4c688
Josh Fox <joshfox87@gmail.com>
2020-02-21 08:05:40 -0500

    themes: Add privapp-permissions needed for ThemePicker
    Change-Id: I676c90c59e5a93bf27adb638126ab03ee14602fb

--- common.mk
+++ common.mk
@@ -5,3 +5,8 @@ include vendor/assets/config/products.mk
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/assets/overlay/common/
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/assets/prebuilt/permissions/product,$(TARGET_COPY_OUT_PRODUCT)/etc/permissions) \
+    $(call find-copy-subdir-files,*,vendor/assets/prebuilt/permissions/system,$(TARGET_COPY_OUT_SYSTEM)/etc/permissions)
+
