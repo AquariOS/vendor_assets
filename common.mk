a9d6f23938e0bf1c45321499f6105d1d1ed25527
calebcabob <calphonic@gmail.com>
2020-02-24 16:23:48 -0500

    assets: Create a packages makefile and move code accordingly
    Change-Id: I532f7daae40506cd89f028f527f2694bd9773c82

--- common.mk
+++ common.mk
@@ -1,11 +1,7 @@
 # Other makefiles
 include vendor/assets/config/sounds.mk
+include vendor/assets/config/products.mk
 
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/assets/overlay/common/
 
-# Common packages
-PRODUCT_PACKAGES += \
-    bootanimation.zip \
-    charger_res_images \
-    product_charger_res_images
