29104d9d9051bc2ec29f0855e6973a57bb1687a2
spezi77 <spezi7713@gmx.net>
2020-02-24 16:58:17 -0500

    themes: Add a bunch of branded accent overlays
    Change-Id: I45650602d65818e06ab3e3c84919d6d1d3ba7631

--- config/accents.mk
+++ config/accents.mk
@@ -19,3 +19,26 @@ PRODUCT_PACKAGES += \
     Red \
     Teal \
     Yellow
+
+# Brand Accents
+PRODUCT_PACKAGES += \
+    AquariOS \
+    AndroidOneGreen \
+    CocaColaRed \
+    DiscordPurple \
+    FacebookBlue \
+    InstagramCerise \
+    JollibeeCrimson \
+    MonsterEnergyGreen \
+    NextbitMint \
+    OneplusRed \
+    PepsiBlue \
+    PocophoneYellow \
+    RazerGreen \
+    SamsungBlue \
+    SpotifyGreen \
+    StarbucksGreen \
+    TwitchPurple \
+    TwitterBlue \
+    XboxGreen \
+    XiaomiOrange
